727f394fb6ce2383f862f7e25a9c30d35037e8f2
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-23 20:57:14 +0100

    Main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,34 @@
 <script>
     export default {
-        name: 'Main'
+        name: 'Main',
+        data() {
+            return {
+                content: '--> Content goes here <--'
+            }
+        }
     }
 </script>
 
 <template>
-
+    <main>
+        <div class="container-big">
+        {{ content }}
+        </div>
+    </main>
 </template>
+
+<style lang="scss" scoped>
+    @use "../components/styles/general.scss" as *;
+
+    main {
+        background-color: #1c1c1c;
+        height: 130px;
+        .container-big {
+            @include my-container(80rem);
+            @include my-flex(flex, start, center);
+            color: #fff;
+            font-size: 2em;
+            height: 100%;
+        }
+    }
+</style>
