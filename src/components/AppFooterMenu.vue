d082b952fc8d2e5ccbe1f365cb7c21f4f000f29b
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-23 22:13:18 +0100

    Footer menu background images

--- src/components/AppFooterMenu.vue
+++ src/components/AppFooterMenu.vue
@@ -5,4 +5,18 @@
 </script>
 
 <template>
+    <div class="background">
+
+    </div>
 </template>
+
+<style lang="scss" scoped>
+    .background {
+        height: 23.125rem;
+        background-image: url('../assets/dc-logo-bg.png'), url('../assets/footer-bg.jpg');
+        background-size: auto, cover;
+        background-repeat: no-repeat;
+        background-position-y: 50%, 0;
+        background-position-x: 85%, 0;
+    }
+</style>
