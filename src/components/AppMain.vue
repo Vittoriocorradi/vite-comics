8408680be033d56a445a45175b92368f9767d7fb
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-24 16:10:47 +0100

    Jumbotron image

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,34 +1,20 @@
 <script>
+    import MainHero from './MainHero.vue';
+
     export default {
         name: 'Main',
-        data() {
-            return {
-                content: '--> Content goes here <--'
-            }
+        components: {
+            MainHero
         }
     }
 </script>
 
 <template>
     <main>
-        <div class="container-big">
-        {{ content }}
-        </div>
+        <MainHero></MainHero>
     </main>
 </template>
 
 <style lang="scss" scoped>
-    @use "../assets/styles/partials/mixins.scss" as *;
 
-    main {
-        background-color: #1c1c1c;
-        height: 130px;
-        .container-big {
-            @include my-container(80rem);
-            @include my-flex(flex, start, center);
-            color: #fff;
-            font-size: 1.6em;
-            height: 100%;
-        }
-    }
 </style>
---
+++ src/components/MainHero.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        name: 'AppHero'
+    }
+</script>
+
+<template>
+    <section class="background">
+
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    .background {
+        height: 25rem;
+        background-image: url('../assets/img/jumbotron.jpg');
+    }
+</style>
