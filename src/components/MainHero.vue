b7f4f3d6356d17cdedb783bcb30b6cc7e6827217
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-24 16:21:21 +0100

    Jumbotron div posizione assoluta

--- src/components/MainHero.vue
+++ src/components/MainHero.vue
@@ -6,13 +6,36 @@
 
 <template>
     <section class="background">
-
+        <div class="container">
+            <h2>current series</h2>
+        </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
+    @use '../assets/styles/partials/variables.scss' as *;
+    @use '../assets/styles/partials/mixins.scss' as *;
     .background {
         height: 25rem;
         background-image: url('../assets/img/jumbotron.jpg');
+        background-size: cover
+    }
+
+    section .container{
+        @include my-container(80rem);
+        position: relative;
+        height: 100%;
+
+        h2 {
+            background-color: $primary-color;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            transform: translate(0, 50%);
+            padding: .9375rem 1.875rem;
+            font-size: 1.625rem;
+            text-transform: uppercase;
+            color: #fff;
+        }
     }
 </style>
